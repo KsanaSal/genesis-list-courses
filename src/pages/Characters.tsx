1efd4661685f888b0804b546514990dae2ff94e3
KsanaSal <oasalivon@gmail.com>
2023-03-13 18:11:42 +0200

    Added get the characters cartoon

--- src/pages/Characters.tsx
+++ src/pages/Characters.tsx
@@ -3,9 +3,26 @@ import rickMortyDes from "../assets/images/rick-morty-desktop.png";
 import css from "./Characters.module.css";
 import SearchInput from "../components/searchInput/SearchInput";
 import CardCharacter from "../components/cardCharacter/CardCharacter";
+import { useEffect, useState } from "react";
+import getSearchCharacters from "../data/getSearchCharacters";
 
 const Characters = ({ loading }: any) => {
+    const [characters, setCharacters] = useState([]);
+
+    useEffect(() => {
+        const getChacters = async () => {
+            try {
+                const fetchCharacters = await getSearchCharacters("as");
+                console.log(fetchCharacters);
+            } catch {
+                console.log("first");
+            }
+        };
+        getChacters();
+    }, []);
+
     console.log(loading);
+
     return (
         <div>
             <img
